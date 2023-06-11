7cfdd879016b27fc96c03c8f93388db8299d31bb
Prabhjot Singh Bhatia <72307426+prabh1303@users.noreply.github.com>
2023-06-11 06:16:39 +0530

    Update urls.py
    updated urls and API's

--- marpu/marpu/urls.py
+++ marpu/marpu/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('interns/', include('interns.urls')),
+    path('alumni/', include('alumni.urls')),
 ]
